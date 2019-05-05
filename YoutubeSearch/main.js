056766db3942905713a8a9aa18ad1337abdba274
nam_45 <nam_1999_x@yahoo.com.vn>
2019-05-05 20:47:52 +0700

    btvn tuan 8

---
+++ YoutubeSearch/main.js
@@ -0,0 +1,59 @@
+var key;
+var pagetoken;
+$("#search").on("submit",function(event){
+    event.preventDefault();
+    key = $("#keyword").val();
+    $(":text").val("");
+    $.ajax({
+        url: `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${key}&type=video&key=AIzaSyA9gQZ-oYomFypZN7PsupZJtOfQqA6Q3qw`,
+        type: "GET",
+        success: function(data){
+            //console.log(data);
+            pagetoken = data.nextPageToken;
+            // console.log(pagetoken);
+            $("#result-list").empty();
+            data.items.forEach(function(value){
+                $("#result-list").append(`
+                <a class="result col-md-12" href="https://youtube.com/watch?v=${value.id.videoId}?autoreplay=true" target="_blank">
+                <img src="${value.snippet.thumbnails.high.url}" alt="">
+                <div class="video_info">
+                    <h2 class="title">${value.snippet.title}</h2>
+                    <p class="description">${value.snippet.description}</p>
+                    <span>View >></span>
+                </div>
+            </a>`)
+            })
+        },
+        error: function(error){
+          console.log(error);
+        }
+      })
+  })
+
+  $(window).scroll( function(event){
+    var scrollHeight = $(document).height();
+	var scrollPosition = $(window).height() + $(window).scrollTop();
+	if ((scrollHeight - scrollPosition) / scrollHeight === 0){
+    $.ajax({
+        url:`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${key}&type=video&key=AIzaSyA9gQZ-oYomFypZN7PsupZJtOfQqA6Q3qw&pageToken=${pagetoken}`,
+        type: "GET",
+        success: function(subdata){
+            subdata.items.forEach(function(subvalue){
+                $("#result-list").append(`
+                <a class="result col-md-12" href="https://youtube.com/watch?v=${subvalue.id.videoId}?autoreplay=true" target="_blank">
+                <img src="${subvalue.snippet.thumbnails.high.url}" alt="">
+                <div class="video_info">
+                    <h2 class="title">${subvalue.snippet.title}</h2>
+                    <p class="description">${subvalue.snippet.description}</p>
+                    <span>View >></span>
+                </div>
+            </a>`)
+            })
+            pagetoken = subdata.nextPageToken;
+        },
+        error:function(error){
+            console.log(error);
+        }
+    })
+    }
+  })
